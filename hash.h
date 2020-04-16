a9cc0d09786148d56cc0c2f5084c434063275995
Megan Mehta <megandeemehta@gmail.com>
2020-04-16 11:08:42 -0500

    Create hash.h

---
+++ hash.h
@@ -0,0 +1,11 @@
+/*hash.h*/
+
+#include <string>
+
+using namespace std;
+
+bool isNumeric(string s);
+int Hash1(string id, int N);
+int Hash2(string abbrev, int N);
+int Hash3(string tripId, int N);
+int Hash4(string bikeId, int N);
